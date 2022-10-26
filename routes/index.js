d9623f0cb1c28c900150b4477fdb5576d6bfef9c
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-10-26 10:10:51 +0300

    add messages parsing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
+router.get("/", (req, res, next) => {
+    res.render("index", { title: "Messages", messages: messages });
+});
+
+router.post("/new", (req, res, next) => {
+    const { messageText, messageUser } = req.body;
+
+    messages.push({
+        text: messageText,
+        user: messageUser,
+        added: new Date(),
+    });
+
+    res.redirect("/");
 });
 
 module.exports = router;
